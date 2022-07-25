c574ccc89e9abcc6b0c473bdf141d619dff83cff
daniel palacios <daniel.palacios@crowdbotics.com>
2022-07-25 15:12:36 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -8,10 +8,10 @@ const Untitled1 = ({
   navigation
 }) => {
   return <View style={{
-    backgroundColor: '#f0f0f1',
+    backgroundColor: "#FFF",
     padding: 10,
-    position: 'relative',
-    height: '100%'
+    position: "relative",
+    height: "100%"
   }}>
       <Text style={{
       left: 143,
